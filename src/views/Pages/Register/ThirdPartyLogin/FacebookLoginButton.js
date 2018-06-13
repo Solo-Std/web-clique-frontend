aa479e4eb7abb22f2fe200c055409745ce0a27ac
Owen <owenyuwono@gmail.com>
2018-06-13 14:52:15 +0700

    facebook and google login is now working

--- src/views/Pages/Register/ThirdPartyLogin/FacebookLoginButton.js
+++ src/views/Pages/Register/ThirdPartyLogin/FacebookLoginButton.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
+import axios from "axios/index";
 
 class FacebookLoginButton extends React.Component {
   responseFacebook(response) {
-    console.log(response)
+    const user = {
+      username: response.id,
+      password: response.id,
+      email: response.email,
+      token: response.accessToken
+    };
+
+    console.log(user);
+    console.log(response);
+    return axios.post(`http://localhost:8000/index.php/api/user_master/fb_login`, { user })
+      .then(res => {
+        if(res === "SUCCESS"){
+          console.log("SUCCESS");
+        }
+        else if(res === "FAILED"){
+          console.log("FAILED");
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      });
   }
 
   render() {
     return (
       <FacebookLogin
         cssClass="btn-facebook"
-        appId="1088597931155576"
+        appId="469693686819110"
         autoLoad={true}
         fields="name,email,picture"
         callback={this.responseFacebook}
--- src/views/Pages/Register/ThirdPartyLogin/GoogleLoginButton.js
+++ src/views/Pages/Register/ThirdPartyLogin/GoogleLoginButton.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import axios from "axios/index";
 
 class GoogleLoginButton extends React.Component{
   responseGoogle(response) {
+    const user = {
+      username: response.googleId,
+      password: response.googleId,
+      email: response.profileObj.email,
+      token: response.accessToken
+    };
+
+    console.log(user);
     console.log(response);
+    return axios.post(`http://localhost:8000/index.php/api/user_master/gp_login`, { user })
+      .then(res => {
+        if(res === "SUCCESS"){
+          console.log("SUCCESS");
+        }
+        else if(res === "FAILED"){
+          console.log("FAILED");
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      });
   };
 
   render(){
     return (
         <GoogleLogin
           className="btn-google-plus"
-          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          clientId="709282008481-2140ljc3dd865ld0rp818lklnh381di8.apps.googleusercontent.com"
           buttonText="Login with Google"
           onSuccess={this.responseGoogle}
           onFailure={this.responseGoogle}
