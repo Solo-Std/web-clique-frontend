4ed21ff0c7d101a7467c21f760d57442bc152f49
Owen <owenyuwono@gmail.com>
2018-06-19 01:12:38 +0700

    fix reply loader

--- src/views/Dashboard/Views/Post.js
+++ src/views/Dashboard/Views/Post.js
@@ -11,11 +11,11 @@ class Feeds extends Component {
 
     this.state = {
       comments: [],
-      replies: [],
       posts: []
     };
 
     this.renderItem = this.renderItem.bind( this );
+    this.renderReplies = this.renderReplies.bind( this );
   }
 
   componentWillMount() {
@@ -24,12 +24,7 @@ class Feeds extends Component {
         let data = [];
         response.data.map( ( content, index ) => data[ index ] = content );
         this.setState( { comments: data } );
-      } );
-    axios.get( `http://localhost:8000/index.php/api/reply_master/` + this.props.id )
-      .then( response => {
-        let data = [];
-        response.data.map( ( content, index ) => data[ index ] = content );
-        this.setState( { replies: data } );
+        console.log(this.state.comments);
       } );
     axios.get( `http://localhost:8000/index.php/api/post_master/` + this.props.id )
       .then( response => {
@@ -62,40 +57,27 @@ class Feeds extends Component {
               </Col>
             </Row>
           </Col>
-          {this.renderComment()}
+          {this.renderReplies(i)}
         </ListGroupItem>
       );
     }
     return data;
   }
 
-  renderComment(){
+  renderReplies(index){
     let data = [];
-    for ( let i = 0; i < this.state.replies.length; i++ ) {
+    for ( let i=0;i<this.state.comments[index]['replies'].length;i++) {
       data.push(
-        <ListGroupItem tag="a" action>
-          <Col xs={"12"}>
-            <Row>
-              <Col sm="12">
-                <span className="font-xs">Posted by <a href="#">@{ this.state.replies[ i ][ 'username' ] }</a></span>
-                <span className="font-xs">&nbsp;<TimeAgo date={ this.state.replies[ i ][ 'date_created' ] }/></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col sm={ "12" }>
-                <span className="font-sm">{ this.state.replies[ i ][ 'reply' ] }<br/></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col sm={ "12" }>
-                <a className="text-black-50" href={ "#" }><i className="fa fa-comment"/> Reply</a>
-                &nbsp;
-                <a className="text-black-50" href={ "#" }><i className="fa fa-thumbs-up"/> Like</a>
-              </Col>
-            </Row>
+          <Col sm={"12"}>
+            <div className="callout">
+              <small className="text-muted">
+                <a href="#">@{ this.state.comments[index]['replies'][i]['username'] }</a>
+                &nbsp;<TimeAgo date={ this.state.comments[index]['replies'][i]['date_created'] }/>
+              </small>
+              <br/>
+              <span>{ this.state.comments[index]['replies'][i]['reply'] }</span>
+            </div>
           </Col>
-
-        </ListGroupItem>
       );
     }
     return data;
